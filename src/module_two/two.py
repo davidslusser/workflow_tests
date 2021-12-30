8baf502bed88b046fddfa82f834b051652a3501b
davidslusser <dbslusser@gmail.com>
2021-12-30 10:47:08 -0800

    add black error

--- src/module_two/two.py
+++ src/module_two/two.py
@@ -1,6 +1,8 @@
-"""This is module two  """
+""" This is module two  """
 
 
 def subtract(var1, var2) -> int:
     """do some math"""
+    msg = 'this is some string'
+    print(msg)
     return var1 - var2
