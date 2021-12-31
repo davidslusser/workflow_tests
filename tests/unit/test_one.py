03a73acd6d06a12419c1b8496eee43a691fc39ef
davidslusser <dbslusser@gmail.com>
2021-12-31 12:23:03 -0800

    adding tests

---
+++ tests/unit/test_one.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+
+class ModuleOneTest(TestCase):
+    """ run some tests on module one code """
+    
+    def test_fake_one(self):
+        self.assertEqual(True, True)
+
+    def test_fake_two(self):
+        self.assertEqual(True, True)
---
+++ tests/unit/test_two.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+
+class ModuleOneTest(TestCase):
+    """ run some tests on module two code """
+    
+    def test_fake_one(self):
+        self.assertEqual(True, True)
+
+    def test_fake_two(self):
+        self.assertEqual(True, True)
