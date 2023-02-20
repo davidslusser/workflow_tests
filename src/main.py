3bf800ca6f046452825b04103495092a158d6270
davidslusser <dbslusser@gmail.com>
2023-02-20 12:23:16 -0800

    adding safety

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
-""" just a test for workflows"""
-
+""" just a test for workflows """
 import sys
 import os
 
 
 def no_op():
-    """a no-op function"""
+    """ a no-op function """
+    # this is an inline comment
+
     print(os.getcwd())
 
 
 def main():
     """script entry point"""
     print("in main()")
+    print('still in main()')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
--- src/module_one/one.py
+++ src/module_one/one.py
@@ -1,10 +1,16 @@
 """this is module one"""
 import requests
 
+SOME_KEY = 'AS5D3GLK3JHS47SAI8DSA8F'
 
-def add(var1: int, var2: int) -> int:
+
+def add(var1: int, var2: int) -> str:
     """do some math"""
     msg = "this is string one"
+    pwd = "some_password"
+    some_key = "mykey"
+    access_key = "22093740239670237024843420327"
+    secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
     print(msg)
     return var1 + var2
 
