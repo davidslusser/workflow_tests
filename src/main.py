4bf93eb60e76aea294c3c5e9a6c78494cda1cb53
David Slusser <dbslusser@gmail.com>
2023-06-17 13:45:47 -0700

    testing pylint

--- src/main.py
+++ src/main.py
@@ -5,13 +5,15 @@ import sys
 
 
 def no_op():
-    """a no-op function"""
+    """a no-op function """
+    test = "this is a very long line...  dfasdflkj alkdsjf awe5a;dnv a;o8e5q3 wdsf an s;lfiaj gcxnbv;akreu tkan v;kauhtroi asb kajsdf;kwe rk;kjn kbasd a"
     print(os.getcwd())
 
 
 def main():
     """script entry point"""
-    print("in main()")
+    print('in main()')
+
 
 
 if __name__ == "__main__":
